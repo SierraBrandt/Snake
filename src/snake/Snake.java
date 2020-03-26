71d1c040b06d8cf1e1a409f246803137779dbf7e
Sierra Brandt <sierrabrandt1@gmail.com>
2020-03-26 15:04:37 -0700

    First commit

---
+++ src/snake/Snake.java
@@ -0,0 +1,283 @@
+
+package snake;
+
+
+import java.io.*; 
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Snake extends JFrame implements Runnable {
+
+    static final int numRows = 21;
+    static final int numColumns = 21;
+    static final int XBORDER = 20;
+    static final int YBORDER = 20;
+    static final int YTITLE = 30;
+    static final int WINDOW_BORDER = 8;
+    static final int WINDOW_WIDTH = 2*(WINDOW_BORDER + XBORDER) + numColumns*30;
+    static final int WINDOW_HEIGHT = YTITLE + WINDOW_BORDER + 2 * YBORDER + numRows*30;
+    
+    boolean animateFirstTime = true;
+    int xsize = -1;
+    int ysize = -1;
+    Image image;
+    Graphics2D g;
+
+    final int EMPTY = 0;
+    final int SNAKE = 1;
+    int board[][];
+
+    int currentRow;
+    int currentColumn;
+    int columnDir;
+    int rowDir;
+    
+    static Snake frame;
+    public static void main(String[] args) {
+        frame = new Snake();
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public Snake() {
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                if (e.BUTTON1 == e.getButton()) {
+                    //left button
+
+// location of the cursor.
+                    int xpos = e.getX();
+                    int ypos = e.getY();
+
+                }
+                if (e.BUTTON3 == e.getButton()) {
+                    //right button
+                    reset();
+                }
+                repaint();
+            }
+        });
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+      public void mouseDragged(MouseEvent e) {
+        repaint();
+      }
+    });
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+      public void mouseMoved(MouseEvent e) {
+
+        repaint();
+      }
+    });
+
+        addKeyListener(new KeyAdapter() {
+
+            public void keyPressed(KeyEvent e) {
+                if (e.VK_UP == e.getKeyCode()) {
+
+                } else if (e.VK_DOWN == e.getKeyCode()) {
+
+                } else if (e.VK_LEFT == e.getKeyCode()) {
+
+                } else if (e.VK_RIGHT == e.getKeyCode()) {
+
+                }
+                repaint();
+            }
+        });
+        init();
+        start();
+    }
+    Thread relaxer;
+////////////////////////////////////////////////////////////////////////////
+    public void init() {
+        requestFocus();
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void destroy() {
+    }
+
+
+
+////////////////////////////////////////////////////////////////////////////
+    public void paint(Graphics gOld) {
+        if (image == null || xsize != getSize().width || ysize != getSize().height) {
+            xsize = getSize().width;
+            ysize = getSize().height;
+            image = createImage(xsize, ysize);
+            g = (Graphics2D) image.getGraphics();
+            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+        }
+//fill background
+        g.setColor(Color.cyan);
+        g.fillRect(0, 0, xsize, ysize);
+
+        int x[] = {getX(0), getX(getWidth2()), getX(getWidth2()), getX(0), getX(0)};
+        int y[] = {getY(0), getY(0), getY(getHeight2()), getY(getHeight2()), getY(0)};
+//fill border
+        g.setColor(Color.white);
+        g.fillPolygon(x, y, 4);
+// draw border
+        g.setColor(Color.red);
+        g.drawPolyline(x, y, 5);
+
+        if (animateFirstTime) {
+            gOld.drawImage(image, 0, 0, null);
+            return;
+        }
+
+        
+        g.setColor(Color.red);
+//horizontal lines
+        for (int zi=1;zi<numRows;zi++)
+        {
+            g.drawLine(getX(0) ,getY(0)+zi*getHeight2()/numRows ,
+            getX(getWidth2()) ,getY(0)+zi*getHeight2()/numRows );
+        }
+//vertical lines
+        for (int zi=1;zi<numColumns;zi++)
+        {
+            g.drawLine(getX(0)+zi*getWidth2()/numColumns ,getY(0) ,
+            getX(0)+zi*getWidth2()/numColumns,getY(getHeight2())  );
+        }
+        
+//Display the objects of the board
+        for (int zrow=0;zrow<numRows;zrow++)
+        {
+            for (int zcolumn=0;zcolumn<numColumns;zcolumn++)
+            {
+//If the location on the board is snake, then draw the box gray.                
+                if (board[zrow][zcolumn] == SNAKE)
+                {
+                    g.setColor(Color.gray);
+                    g.fillRect(getX(0)+zcolumn*getWidth2()/numColumns,
+                    getY(0)+zrow*getHeight2()/numRows,
+                    getWidth2()/numColumns,
+                    getHeight2()/numRows);
+                }  
+
+            }
+        }
+
+        gOld.drawImage(image, 0, 0, null);
+    }
+
+////////////////////////////////////////////////////////////////////////////
+// needed for     implement runnable
+    public void run() {
+        while (true) {
+            animate();
+            repaint();
+            
+            
+            double seconds = .04;    //time that 1 frame takes.
+            int miliseconds = (int) (1000.0 * seconds);
+            try {
+                Thread.sleep(miliseconds);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+/////////////////////////////////////////////////////////////////////////
+    public void reset() {
+
+//Allocate memory for the 2D array that represents the board.
+        board = new int[numRows][numColumns];
+//Initialize the board to be empty.
+        for (int zrow = 0;zrow < numRows;zrow++)
+        {
+            for (int zcolumn = 0;zcolumn < numColumns;zcolumn++)
+                board[zrow][zcolumn] = EMPTY;
+        }
+//Set row 2, column 3 to be a snake box that will be drawn gray.
+        board[2][3] = SNAKE;
+        board[2][4] = SNAKE;
+        board[2][5] = SNAKE;
+        board[2][6] = SNAKE;
+        board[3][2] = SNAKE;
+        board[4][2] = SNAKE;
+        board[5][3] = SNAKE;
+        board[5][4] = SNAKE;
+        board[5][5] = SNAKE;
+        board[5][6] = SNAKE;
+        board[6][7] = SNAKE;
+        board[7][7] = SNAKE;
+        board[8][6] = SNAKE;
+        board[8][5] = SNAKE;
+        board[8][4] = SNAKE;
+        board[8][3] = SNAKE;
+        
+        board[2][10] = SNAKE;
+        board[2][11] = SNAKE;
+        board[2][12] = SNAKE;
+        board[3][10] = SNAKE;
+        board[3][13] = SNAKE;
+        board[4][10] = SNAKE;
+        board[4][13] = SNAKE;
+        board[5][11] = SNAKE;
+        board[5][12] = SNAKE;
+        board[5][10] = SNAKE;
+        board[6][10] = SNAKE;
+        board[6][13] = SNAKE;
+        board[7][10] = SNAKE;
+        board[7][13] = SNAKE;
+        board[8][10] = SNAKE;
+        board[8][11] = SNAKE;
+        board[8][12] = SNAKE;
+    }
+/////////////////////////////////////////////////////////////////////////
+    public void animate() {
+        if (animateFirstTime) {
+            animateFirstTime = false;
+            if (xsize != getSize().width || ysize != getSize().height) {
+                xsize = getSize().width;
+                ysize = getSize().height;
+            }
+            reset();
+        }
+        
+        
+    }
+
+////////////////////////////////////////////////////////////////////////////
+    public void start() {
+        if (relaxer == null) {
+            relaxer = new Thread(this);
+            relaxer.start();
+        }
+    }
+////////////////////////////////////////////////////////////////////////////
+    public void stop() {
+        if (relaxer.isAlive()) {
+            relaxer.stop();
+        }
+        relaxer = null;
+    }
+
+
+/////////////////////////////////////////////////////////////////////////
+    public int getX(int x) {
+        return (x + XBORDER + WINDOW_BORDER);
+    }
+
+    public int getY(int y) {
+        return (y + YBORDER + YTITLE );
+    }
+
+    public int getYNormal(int y) {
+        return (-y + YBORDER + YTITLE + getHeight2());
+    }
+    
+    public int getWidth2() {
+        return (xsize - 2 * (XBORDER + WINDOW_BORDER));
+    }
+
+    public int getHeight2() {
+        return (ysize - 2 * YBORDER - WINDOW_BORDER - YTITLE);
+    }
+}
